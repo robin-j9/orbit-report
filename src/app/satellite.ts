6b27d02e2ae6a3136f7bba35779f8eb7a62006a5
Robin Jun <robinjun@Robins-MacBook-Pro-2.local>
2019-12-09 20:19:30 -0600

    added shouldShowWarning method

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,7 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+    
 
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
@@ -12,5 +13,13 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning() {
+        if (this.type.toLowerCase() === 'space debris') {
+            return true;
+        } else {
+            return false;
+        }
+        }
 }
 
